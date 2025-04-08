4529a883007c161685f5f7a14bf18328383443fd
sivagurubilli <gurusrija123@gmail.com>
2025-04-08 18:39:17 +0530

    added changes to this

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,9 +22,9 @@ router.post(
 
 router.post("/v1/user/add-address",authenticateToken, UserAdressController.addUserAddress);
 router.get("/v1/user/get-address",authenticateToken, UserAdressController.getUserAddresses);
-router.put("/v1/user/update-address", authenticateToken,UserAdressController.updateUserAddress);
-router.delete("/v1/user/delete-address", authenticateToken,UserAdressController.deleteUserAddress);
-router.put("/v1/user/address/set-default", authenticateToken,UserAdressController.setDefaultAddress);
+router.post("/v1/user/update-address", authenticateToken,UserAdressController.updateUserAddress);
+router.post("/v1/user/delete-address", authenticateToken,UserAdressController.deleteUserAddress);
+router.post("/v1/user/address/set-default", authenticateToken,UserAdressController.setDefaultAddress);
 
 router.get("/v1/user/get-recomended-products",authenticateToken, UserController.getUserRecommendedProducts);
 
